04e54e6ef5af406b6b19370524c3b3a93f18cc46
Zack Levin <112544104+TheRealPlanZ@users.noreply.github.com>
2023-02-19 14:03:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('../routes/apiRoutes');
+const htmlRoutes = require('../routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
